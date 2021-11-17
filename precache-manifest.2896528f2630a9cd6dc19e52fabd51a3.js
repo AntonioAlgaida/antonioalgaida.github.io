9deb474805307916ef3f25ea2873fd8146c93e80
Antonio Guillen-Perez <antonio_algaida@hotmail.com>
2021-11-16 21:10:24 -0800

    v2

---
+++ precache-manifest.2896528f2630a9cd6dc19e52fabd51a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c1655c5bf249a0b16b7af8dfe93d1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2055484eeac1ffb82d7d",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ab8cd6ce7361f7a34348",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "2055484eeac1ffb82d7d",
+    "url": "/static/js/2.df952924.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.df952924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8cd6ce7361f7a34348",
+    "url": "/static/js/main.6265a113.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
